91b2ad14f1be3e70e97f74078d050433f10ef38f
IwanPWK <koadhyow@gmail.com>
2024-01-15 09:05:21 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MusicApp());
+}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Music App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
